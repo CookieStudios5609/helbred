816f38c0dbe28020c498a4663896011c4e69bed4
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2023-07-05 21:02:51 -0400

    Feat: Begin creating homepage
    In this commit, I begin the homepage by adding material components as a dependency and adding the indicator circle to MainActivity.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 
 ## Installation
 1. Clone the repo using `git clone`
-2. Import the project using Android Studio 2022.2.1 Patch 2 or newer
-3. Perform a Gradle sync and resolve any Java version-related issues
+2. Import the project using Android Studio 2022.2.1 Patch 2 or newer and Java 17.0.7 or newer
+3. Perform a Gradle sync and resolve any version-mismatch issues (hopefully none)
--- app/src/main/java/com/team03/helbred/MainActivity.kt
+++ app/src/main/java/com/team03/helbred/MainActivity.kt
@@ -2,10 +2,24 @@ package com.team03.helbred
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progressWheel = findViewById<CircularProgressIndicator>(R.id.dailyProgressWheel)
+        val progressMessage = findViewById<TextView>(R.id.goalsMessage)
+        progressWheel.setProgress(22, true)
+        if (progressWheel.progress > 20) {
+            progressWheel.setIndicatorColor(getColor(R.color.success_green))
+            progressMessage.setTextColor(getColor(R.color.success_green))
+        }
+
+
+
     }
 }

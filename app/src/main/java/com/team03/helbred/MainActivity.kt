3ae77bb9cc15037a5dd0bd78dcd787bf0ffcc807
T440p <78653311+CookieStudios5609@users.noreply.github.com>
2023-07-10 11:27:05 -0400

    Chore: Refactor annoying namings, prepare later work.
    In this commit, I clear up warnings by exporting the textviews to string resources, prepare to add explicit intents for when the activities are made, and clean up some inconsistent id names.

--- app/src/main/java/com/team03/helbred/MainActivity.kt
+++ app/src/main/java/com/team03/helbred/MainActivity.kt
@@ -2,6 +2,7 @@ package com.team03.helbred
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.progressindicator.CircularProgressIndicator
@@ -15,16 +16,24 @@ class MainActivity : AppCompatActivity() {
         val progressMessage = findViewById<TextView>(R.id.goalsMessage)
         progressWheel.setProgress(22, true)
         val percent = findViewById<TextView>(R.id.percentageText)
-        percent.text = "22%"
+        percent.text = "22%" //TODO: collaborator implements progress/streak calculation
 
-        if (progressWheel.progress > 20) {
+        if (progressWheel.progress > 20) { //TODO: collaborator implements success thresholds
             progressWheel.setIndicatorColor(getColor(R.color.success_green))
             progressMessage.setTextColor(getColor(R.color.success_green))
             progressMessage.text = buildString {
         append("You've made some progress today. Keep going!")
     }
         }
-
+        //TODO: placeholder for future activities
+//        fun onMainButtonsPressed(button: ImageButton) {
+//            if (button.id.toString() == "workout_btn") {
+//                startActivity(Intent(this, WorkoutPickActivity::class.kt))
+//            }
+//        }
+//        fun gotoSettings() {
+//            startActivity(Intent(this))
+//        }
 
 
     }

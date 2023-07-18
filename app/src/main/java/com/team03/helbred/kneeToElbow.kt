90ea52f710f0ef0f7453048d7d30abbbd1536f59
kiki0426 <111385309+kiki0426@users.noreply.github.com>
2023-07-18 13:39:04 -0400

    finished workout page and connected the Imageview to a new activity

---
+++ app/src/main/java/com/team03/helbred/kneeToElbow.kt
@@ -0,0 +1,11 @@
+package com.team03.helbred
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kneeToElbow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_knee_to_elbow)
+    }
+}

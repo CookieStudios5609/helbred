4bc3d99ceeea1750875b1f7c4c5993f0103960d7
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2023-07-17 21:31:05 -0400

    Feat: Add success thresholds, begin work on local storage, begin work on streak calculation

--- app/src/main/java/com/team03/helbred/MainActivity.kt
+++ app/src/main/java/com/team03/helbred/MainActivity.kt
@@ -7,32 +7,50 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.progressindicator.CircularProgressIndicator
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val hydrateButton : ImageButton = findViewById(R.id.hydrate_btn)
-        val workoutsButton = findViewById<ImageButton>(R.id.workout_btn)
-
+        setListeners()
+        showProgress(72)
 
 
-        val progressWheel = findViewById<CircularProgressIndicator>(R.id.dailyProgressWheel)
-        val progressMessage = findViewById<TextView>(R.id.goalsMessage)
-        progressWheel.setProgress(22, true)
-        val percent = findViewById<TextView>(R.id.percentageText)
-        percent.text = "22%" //TODO: collaborator implements progress/streak calculation
 
-        if (progressWheel.progress > 20) { //TODO: collaborator implements success thresholds
-            progressWheel.setIndicatorColor(getColor(R.color.success_green))
-            progressMessage.setTextColor(getColor(R.color.success_green))
-            progressMessage.text = buildString {
-        append("You've made some progress today. Keep going!")
     }
+    private fun getThresholds(): ArrayList<Int>{ //lets just hope no bad info gets in here
+        val prefs = File(applicationContext.filesDir, "pref")
+        if (!prefs.exists()) {
+            val prefWriter = prefs.bufferedWriter()
+            prefWriter.write("25,50,75,100")
+            prefWriter.close()
         }
+        val prefReader = prefs.bufferedReader()
+        val threshs = prefReader.readLine().split(",")
+        val threshes: ArrayList<Int> = ArrayList<Int>()
+        for (strang in threshs) {
+            threshes.add(strang.toInt())
+        }
+        prefReader.close()
+        return threshes
 
+    }
+    fun getStreak(): String {
+        // the streak is just a number. Increment it each time user hits 100
+        val streak = File(applicationContext.filesDir, "streak").bufferedReader()
+        val dayCount:String = streak.readLine()
+        streak.close()
+        return dayCount
+
+    }
+    fun setListeners() {
+        val hydrateButton = findViewById<ImageButton>(R.id.hydrate_btn)
+        val workoutsButton = findViewById<ImageButton>(R.id.workout_btn)
+        val meditateButton = findViewById<ImageButton>(R.id.meditation_btn)
+        val stretchButton = findViewById<ImageButton>(R.id.stretch_btn)
+        val streakButton = findViewById<CircularProgressIndicator>(R.id.dailyProgressWheel)
 
         hydrateButton.setOnClickListener{
             val intent = Intent(this,DrinkingActivity::class.java)
@@ -41,8 +59,49 @@ class MainActivity : AppCompatActivity() {
         workoutsButton.setOnClickListener{
             startActivity(Intent(this, WorkoutLocaleChooserActivity::class.java))
         }
+    }
+    fun showProgress(completion: Int) {
+        val progressWheel = findViewById<CircularProgressIndicator>(R.id.dailyProgressWheel)
+        val progressMessage = findViewById<TextView>(R.id.goalsMessage)
+        progressWheel.setProgress(completion, true)
+        val percent = findViewById<TextView>(R.id.percentageText)
+        percent.text = completion.toString() + "%"
 
-
+        val threshes = getThresholds()
+        if (completion == threshes[3]) { //TODO: collaborator implements success thresholds
+            progressWheel.setIndicatorColor(getColor(R.color.success_green))
+            progressMessage.setTextColor(getColor(R.color.success_green))
+            progressMessage.text = buildString {
+                append("Another day complete. Congratulations!")
+            }
+        }
+        else if (completion > threshes[2]) {
+            progressWheel.setIndicatorColor(getColor(R.color.success_green))
+            progressMessage.setTextColor(getColor(R.color.success_green))
+            progressMessage.text = buildString {
+                append("Just a bit more. Keep going!")
+            }
+        }
+        else if (completion > threshes[1]) {
+            progressWheel.setIndicatorColor(getColor(R.color.success_green))
+            progressMessage.setTextColor(getColor(R.color.success_green))
+            progressMessage.text = buildString {
+                append("You've got this, keep pushing!")
+            }
+        }
+        else if (completion > threshes[0]) {
+            progressWheel.setIndicatorColor(getColor(R.color.black))
+            progressMessage.setTextColor(getColor(R.color.black))
+            percent.setTextColor(getColor(R.color.black))
+            progressMessage.text = buildString {
+                append("Improvement begins here and now.")
+            }
+        }
+        else if (completion < threshes[2]) {
+            progressMessage.text = buildString {
+                append("A fresh day. Time to make history!")
+            }
+        }
 
     }
 }

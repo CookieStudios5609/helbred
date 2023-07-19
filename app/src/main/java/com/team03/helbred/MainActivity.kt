fc1946e6e4ba1b989125d278aee807ebb5a083fb
T440p <78653311+CookieStudios5609@users.noreply.github.com>
2023-07-19 18:44:12 -0400

    Feat: begin creating files to store progress

--- app/src/main/java/com/team03/helbred/MainActivity.kt
+++ app/src/main/java/com/team03/helbred/MainActivity.kt
@@ -3,11 +3,13 @@ package com.team03.helbred
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import java.io.File
+import java.util.Scanner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setListeners()
-        showProgress(79)
+        showProgress(getCompletion()[0])
 
     }
     private fun getThresholds(): ArrayList<Int>{ //lets just hope no bad info gets in here
@@ -33,17 +35,55 @@ class MainActivity : AppCompatActivity() {
         }
         prefReader.close()
         return threshes
+    }
 
-    }//TODO: preferences page
-    private fun getStreak(): String {
+    //TODO: preferences page
+    private fun getCompletion(): ArrayList<Int> {
         // the streak is just a number. Increment it each time user hits 100
         // I think this is a good compromise; making a file that grows every single day could get problematic...
         // though, we'd just be iterating over n days where app has been opened
-        val streak = File(applicationContext.filesDir, "streak").bufferedReader()
-        val dayCount:String = streak.readLine()
-        streak.close()
-        return dayCount
+//        val streak = File(applicationContext.filesDir, "streak").bufferedReader()
+//        val dayCount:String = streak.readLine()
+//        streak.close()
+//        return dayCount
 
+        //I take it back. {creation_timestamp} {total} {water} {workout} {stretch} {meditate}
+        val streakfile = File(applicationContext.filesDir, "streak")
+        if (!streakfile.exists()) {
+            val make = streakfile.bufferedWriter()
+            make.write(String.format("%d 0 0 0 0 0", DateUtils.DAY_IN_MILLIS))
+        }
+        val streak = streakfile.bufferedReader()
+
+        val out = ArrayList<Int>();
+        for (line in streak.lines()) {
+            val scanner = Scanner(line)
+            if (DateUtils.isToday(scanner.nextLong())) {
+                out.add(scanner.nextInt())
+                out.add(scanner.nextInt())
+                out.add(scanner.nextInt())
+                out.add(scanner.nextInt())
+                out.add(scanner.nextInt())
+                streak.close()
+                return out
+            }
+        } // if it never finds today, write one!
+        streak.close()
+        val writer = File(applicationContext.filesDir, "streak").bufferedWriter()
+        writer.newLine()
+            writer.write(
+                String.format("%d %d %d %d %d %d", DateUtils.DAY_IN_MILLIS, 0, 0, 0, 0, 0))
+        writer.close()
+        out.add(0)
+        out.add(0)
+        out.add(0)
+        out.add(0)
+        out.add(0)
+        return out
+    }
+    private fun writeTotals(index: Int, value: Int) {
+        val info = File(applicationContext.filesDir, "streak").bufferedReader()
+        info.close()
     }
     private fun setListeners() {
         val hydrateButton = findViewById<ImageButton>(R.id.hydrate_btn)

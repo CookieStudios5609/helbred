ee96be99c31242b0d326d5e3258a65c1d22800b9
Manggoz <99453089+Manggoz@users.noreply.github.com>
2023-07-20 15:58:21 -0400

    timer is finished
    Need to work on effects and looks of the app

--- app/src/main/java/com/team03/helbred/MeditationActivity.kt
+++ app/src/main/java/com/team03/helbred/MeditationActivity.kt
@@ -10,57 +10,124 @@ import android.widget.TextView
 class MeditationActivity : AppCompatActivity() {
 
     private lateinit var timer: CountDownTimer
-
-
+    private var totalTimePassed: Long = 0
+    private val totalRoutineTime: Long = 40000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meditation)
 
         val TimerBar: ProgressBar = findViewById(R.id.TimerBar)
-        val TimerText:TextView = findViewById(R.id.TimerText)
-        val TimerButton : Button = findViewById(R.id.TimerButton)
-        var BreathingType : TextView = findViewById(R.id.BreathingText)
+        val TimerText: TextView = findViewById(R.id.TimerText)
+        val TimerButton: Button = findViewById(R.id.TimerButton)
+        var BreathingType: TextView = findViewById(R.id.BreathingText)
 
-        TimerBar.max = 7000
+
+
+        TimerBar.max = 5000
         TimerBar.progress = 0
 
 
 
-    timer = object : CountDownTimer(7000,10){
-        override fun onTick(remaining: Long) {
-            TimerText.text=remaining.toString()
-            TimerBar.progress = 7000 - remaining.toInt()
+        timer = object : CountDownTimer(5000, 10) {
+            override fun onTick(remaining: Long) {  //Breath In
+                TimerText.text = remaining.toString()
+                TimerBar.progress = 5000 - remaining.toInt()
+
+            }
+
+            override fun onFinish() {
+                TimerBar.progress = 0
+                BreathingType.text = "Hold"
+                timer.start()
+
+            }
 
         }
-
-        override fun onFinish() {
-            TimerText.text="Done!"
-        }
-
-    }
         TimerButton.setOnClickListener {
-            BreathingExcerise(BreathingType)
+            startBreathingRoutine(BreathingType, TimerText, TimerBar)
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
 
     }
-    override fun onStop(){
+
+
+
+    override fun onStop() {
         super.onStop()
         timer.cancel()
     }
 
+    private fun startBreathingRoutine(BreathingType: TextView, TimerText: TextView, TimerBar: ProgressBar) {
+        timer = object : CountDownTimer(totalRoutineTime - totalTimePassed, 20000) {
+            override fun onTick(remaining: Long) {
+                totalTimePassed = totalRoutineTime - remaining
+                BreathingExercise(BreathingType, TimerText, TimerBar)
+            }
 
-    fun BreathingExcerise(BreathingType: TextView){
+            override fun onFinish() {
+                BreathingType.text = "Done!"
+                TimerText.text="0"
+            }
+        }
+        timer.start()
+    }
+
+    fun BreathingExercise(BreathingType: TextView, TimerText: TextView, TimerBar: ProgressBar) {
+        // Phase 1: Breath In
         BreathingType.text = "Breath In"
-        timer.start()
+        timer = object : CountDownTimer(5000, 10) {
+            override fun onTick(remaining: Long) {
+                TimerText.text = remaining.toString()
+                TimerBar.progress = 5000 - remaining.toInt()
+            }
 
-        BreathingType.text = "Hold"
-        timer.start()
+            override fun onFinish() {
+                // Phase 2: Hold
+                BreathingType.text = "Hold"
+                timer = object : CountDownTimer(5000, 10) {
+                    override fun onTick(remaining: Long) {
+                        TimerText.text = remaining.toString()
+                        TimerBar.progress = 5000 - remaining.toInt()
+                    }
 
+                    override fun onFinish() {
+                        // Phase 3: Breath Out
+                        BreathingType.text = "Breath Out"
+                        timer = object : CountDownTimer(5000, 10) {
+                            override fun onTick(remaining: Long) {
+                                TimerText.text = remaining.toString()
+                                TimerBar.progress = 5000 - remaining.toInt()
+                            }
+
+                            override fun onFinish() {
+                                // Phase 4: Hold
+                                BreathingType.text = "Hold"
+                                timer = object : CountDownTimer(5000, 10) {
+                                    override fun onTick(remaining: Long) {
+                                        TimerText.text = remaining.toString()
+                                        TimerBar.progress = 5000 - remaining.toInt()
+                                    }
+
+                                    override fun onFinish() {
+                                        TimerText.text = "Done!"
+                                    }
+                                }
+                                // Start Phase 4
+                                timer.start()
+                            }
+                        }
+                        // Start Phase 3
+                        timer.start()
+                    }
+                }
+                // Start Phase 2
+                timer.start()
+            }
+        }
+        // Start Phase 1
+        timer.start()
     }
 
 }
+
+

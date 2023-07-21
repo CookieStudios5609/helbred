cafd58a89356eed35e155dbe93c505417419508e
Ikenna Okeoma <iokeoma1@student.gsu.edu>
2023-07-21 12:54:59 -0400

    Reminder app
    An app that reminds you to drink water and workout for the day when you click on the button

---
+++ app/src/main/java/com/team03/helbred/ReminderActivity.kt
@@ -0,0 +1,22 @@
+package com.team03.helbred
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class ReminderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun drinkWater(view: View) {
+        Toast.makeText(this, "Don't forget to drink water throughout the day!", Toast.LENGTH_SHORT).show()
+    }
+
+    fun workout(view: View) {
+        Toast.makeText(this, "It's time to workout!", Toast.LENGTH_SHORT).show()
+    }
+}
